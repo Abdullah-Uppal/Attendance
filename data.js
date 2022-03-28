825f817fb84d4f252ffda27e2dcda34c9b553f92
ahtisham774 <77314350+ahtisham774@users.noreply.github.com>
2022-03-28 21:45:31 +0500

    data.js

---
+++ data.js
@@ -0,0 +1,227 @@
+response = [
+    {
+      RegistrationNumber: "2020-CS-52",
+      Name:"Muhammad Abdullah Uppal",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-53",
+      Name:"Sameed Bin Adeel",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-54",
+      Name:"Fizza Akram",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-55",
+      Name:"Bushra Farooq",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-56",
+      Name:"Zain Rasool",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-57",
+      Name:"Mian Zain Ul Tahir",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-58",
+      Name:"Esha Tanveer",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-59",
+      Name:"Humna",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-61",
+      Name:"Muhammad Umer Amjad",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-62",
+      Name:"Muhammad Awais Yousaf Butt",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-63",
+      Name:"M.Abdullah Butt",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-64",
+      Name:"Qazi Maaz Sajjad",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-65",
+      Name:"Ahtisham Ul Haq",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-66",
+      Name:"Ayesha Shabbir",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-67",
+      Name:"Aysha javaid",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-68",
+      Name:"Rafia Mehmood",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-69",
+      Name:"Ayesha Amjad",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-70",
+      Name:"Khadija Dar",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-71",
+      Name:"Esha Tur Razia",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-72",
+      Name:"Abdullah Yaqub",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-73",
+      Name:"Ridsa Shabbir",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-74",
+      Name:"Muhammad Ahmad",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-75",
+      Name:"Muhammad Khuzaima Umair",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-76",
+      Name:"Hafiz Rehan",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-77",
+      Name:"Taha Hussain Zain",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-79",
+      Name:"Muhammad Usman Rasheed",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-80",
+      Name:"Hussain Shahzad",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-81",
+      Name:"Hafiz Mohsin Raza",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-82",
+      Name:"Adeel Shabbir",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-83",
+      Name:"Laibah Azhar",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-84",
+      Name:"Sana Shafique",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-85",
+      Name:"Salahuddin",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-86",
+      Name:"Talha Ramzan",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-87",
+      Name:"Sadia Ghani",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-88",
+      Name:"Shafqat Ullah",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-89",
+      Name:"Khadija Iqbal",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-90",
+      Name:"Muammad Moiz Wajahat",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-91",
+      Name:"Syed Muattar Mehdi Zaidi",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-92",
+      Name:"Muhammad Aqib Shahzad",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-93",
+      Name:"Syed Bilal Hashmi",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-95",
+      Name:"Muhammad Ahmed",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-97",
+      Name:"Momina Ghauri",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-98",
+      Name:"Asad Hussain",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-CS-99",
+      Name:"Eman Muneeb",
+      Status:"none"
+    },
+    {
+      RegistrationNumber: "2020-R\/2018-CS-79",
+      Name:"Haris Bajwa",
+      Status:"none"
+    }
+  ];
